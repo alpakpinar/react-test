88166e0ae700ffd4fff49d64707713988088dd11
Alp Akpinar <aakpinar@bu.edu>
2021-02-12 19:23:10 -0500

    Keyboard functionality for searchbar/dropdown

--- src/pages/HomePage/components/ProfileLandingPage.js
+++ src/pages/HomePage/components/ProfileLandingPage.js
@@ -20,6 +20,8 @@ class ProfileLandingPage extends React.Component {
 
         this.chatRoomDropdown = this.chatRoomDropdown.bind(this)
         this.removeChatRoomDropdown = this.removeChatRoomDropdown.bind(this)
+        this.setChatRoom = this.setChatRoom.bind(this)
+        this.changeActiveChatRoomOnKeystreak = this.changeActiveChatRoomOnKeystreak.bind(this)
 
     }
     componentDidMount() {
@@ -73,19 +75,76 @@ class ProfileLandingPage extends React.Component {
         })
     }
 
+    setChatRoom(e) {
+        e.preventDefault()
+        this.searchbar.current.value = e.target.text
+        // Close the dropdown
+        this.removeChatRoomDropdown()
+        this.searchbar.current.focus()
+    }
+
+    changeActiveChatRoomOnKeystreak(e) {
+        const chatroomInputField = this.searchbar.current.value
+        if (chatroomInputField === '') {
+            return
+        }
+        const currentActiveTab = this.state.dropdown.activeTabId
+        const currentChatroomList = this.state.dropdown.chatroomlist
+
+        if (e.key === 'Enter') {
+            if (currentChatroomList.length ===  0) {
+                return
+            }
+            // Do not submit the form because of the enter keypress lol
+            e.preventDefault()
+            const selectedChatroom = currentChatroomList[currentActiveTab]
+            this.searchbar.current.value = selectedChatroom.name
+            this.removeChatRoomDropdown()
+            return
+        }
+
+        // Escape key
+        if (e.keyCode === 27) {
+            this.removeChatRoomDropdown()
+            return
+        }
+        // Update the active tab ID, based on whether up or down key is pressed
+        // Up key
+        let newActiveTab
+        if (e.keyCode === 38) {
+            if (currentActiveTab < 0) {
+                return
+            }
+            newActiveTab = currentActiveTab - 1
+        }
+        // Down key
+        else if (e.keyCode === 40) {
+            newActiveTab = currentActiveTab + 1
+        }
+
+        // Update the state
+        this.setState({
+            ...this.state,
+            dropdown: {
+                chatroomlist: currentChatroomList,
+                activeTabId: newActiveTab
+            }
+        })
+    }
+
     render() {
         return (
             <div>
                 <div className="search-bar-container">
                     <h1>Topluluk keşfet</h1>
-                    <input className="profile-landing-page-search-bar" placeholder="Topluluk ara" ref={this.searchbar} onChange={this.chatRoomDropdown} />
+                    <input className="profile-landing-page-search-bar" placeholder="Topluluk ara" ref={this.searchbar} onChange={this.chatRoomDropdown} onKeyDown={this.changeActiveChatRoomOnKeystreak} />
                     <div className="chatroom-dropdown-menu">
                         {this.state.dropdown.chatroomlist.map((chatroom, index) => {
                             if (index === this.state.dropdown.activeTabId) {
-                                return <a className="chatroom-suggestion-item chatroom-item-active">{chatroom.name}</a>
+                                return <a onClick={this.setChatRoom} className="chatroom-suggestion-item chatroom-item-active">{chatroom.name}</a>
                             }
                             else {
-                                return <a className="chatroom-suggestion-item">{chatroom.name}</a>
+                                return <a onClick={this.setChatRoom} className="chatroom-suggestion-item">{chatroom.name}</a>
                             }
                         })}
                     </div>
