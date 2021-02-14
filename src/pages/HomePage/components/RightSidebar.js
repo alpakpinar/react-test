78c972f2dff497b9794d2aaae98a98066ed98b0e
Alp Akpinar <aakpinar@bu.edu>
2021-02-14 01:05:31 -0500

    User menu

--- src/pages/HomePage/components/RightSidebar.js
+++ src/pages/HomePage/components/RightSidebar.js
@@ -6,13 +6,48 @@ import List from '@material-ui/core/List'
 import ListItem from '@material-ui/core/ListItem'
 import ListItemIcon from '@material-ui/core/ListItemIcon'
 import ListItemText from '@material-ui/core/ListItemText'
+import Menu from '@material-ui/core/Menu'
+import MenuItem from '@material-ui/core/MenuItem'
 
+class UserMenu extends React.Component {
+    render() {
+        return (
+            <div>
+                <Menu
+                    anchorEl={this.props.anchorForMenu}
+                    keepMounted
+                    open={Boolean(this.props.anchorForMenu)}
+                    onClose={this.props.handleClose}
+                >
+                    <MenuItem onClick={this.props.handleClose}>Direkt mesaj yolla</MenuItem>
+                    <MenuItem onClick={this.props.handleClose}>Kullanıcıyı ekle</MenuItem>
+                </Menu>
+            </div>
+        )
+    }
+}
 
 class RightSidebar extends React.Component {
     constructor(props) {
         super(props)
-
+        this.state = {
+            anchorForMenu: null
+        }
         this.renderContacts = this.renderContacts.bind(this)
+        this.renderUserMenu = this.renderUserMenu.bind(this)
+        this.handleClose = this.handleClose.bind(this)
+    }
+
+    renderUserMenu(e) {
+        this.setState({
+            anchorForMenu: e.target
+        })
+    }
+    
+    handleClose() {
+        this.setState({
+            anchorForMenu: null
+        })
     }
 
     renderContacts() {
@@ -20,12 +55,13 @@ class RightSidebar extends React.Component {
             const username = contact.username
             return (
                 <List>
-                    <ListItem button>
+                    <ListItem button onClick={this.renderUserMenu}>
                         <ListItemIcon>
                             <Avatar>{username[0].toUpperCase()}</Avatar>
                         </ListItemIcon>
                         <ListItemText primary={username}/>
                     </ListItem>
+                    <UserMenu anchorForMenu={this.state.anchorForMenu} handleClose={this.handleClose} />
                 </List>
             )
         })
